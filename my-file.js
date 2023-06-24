cdac78718c72bada1115787957825cedaf09889d
divanshi16 <divanshikoushik@gmail.com>
2023-06-24 23:14:58 +0530

    typed new styling

---
+++ my-file.js
@@ -0,0 +1,9 @@
+const savechanges = () => {
+  const taskData = {
+    id: `${Date.now()}`
+    imageUrl: document.getElementById("imageurl "),
+    taskTitle: document.getElementById("tasktitle"),
+    taskType: document.getElementById("tasktype "),
+    taskDescription: document.getElementById("taskdescription ")
+  }
+};
